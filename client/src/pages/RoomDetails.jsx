a5de5ec2bf948818099d10dcace2a6052357c649
RockyR6 <rockyroy76r@gmail.com>
2025-11-06 10:13:50 +0530

    backend added

--- client/src/pages/RoomDetails.jsx
+++ client/src/pages/RoomDetails.jsx
@@ -16,7 +16,7 @@ const RoomDetails = () => {
     },[])
 
   return room && (
-    <div className='py-28 md:py-35 px-4 md:px-16 lg:px-24 xl:px-32'>
+    <div className='py-28 md:py-35 px-4 md:px-16 lg:px-24 xl:px-32 bg-amber-300'>
       {/* Room Details */}
       <div className='flex flex-col md:flex-row items-start md:items-center gap-2'>
         <h1 className='text-3xl md:text-4xl font-playfair'>{room.hotel.name}<span className='font-inter text-sm'>({room.roomType})</span></h1>
